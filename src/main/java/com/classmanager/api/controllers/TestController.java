de1f1e9cdc2e18c2cbfceeb76e478325911d40d5
Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-08-14 01:17:09 -0300

    Configurations fix - pom.xml

--- src/main/java/com/classmanager/api/controllers/TestController.java
+++ src/main/java/com/classmanager/api/controllers/TestController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(name = "/test")
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/")
     public String TestController(){
-        return "Test";
+        return "Willkommen bei server!";
     }
 }
