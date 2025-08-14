c618ff1ad4535ff1cff3cff1132601ca11f8a2cd
Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-08-13 21:22:09 -0300

    Containers creation + application.yml config

---
+++ src/main/java/com/classmanager/api/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.classmanager.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/test")
+public class TestController {
+
+    @GetMapping
+    public String TestController(){
+        return "Test";
+    }
+}
